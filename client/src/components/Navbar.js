3d92435b40d73492ef7e65b18fc2c796ac07a0ef
Luxin Zhang <67583508+LuxinZhang902@users.noreply.github.com>
2023-08-19 23:18:40 -0400

    add theme.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import {Box, Link, Typography} from '@mui/material'
+import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <Box width={'100%'} p="1rem 6%" textAligh={'center'} sx={{boxShadow:3, mb:2, bgcolor:'purple', color:'white'}}>
-      <h1>Nav menu</h1>
+      <Typography variant="h1" color={"white"} fontWeight="bold">  
+        AI GPT3 Clone
+      </Typography>
+      <NavLink to="/register" p={1}>
+        Sign up
+      </NavLink>
+      <NavLink to="/login" p={1}>
+        Sign In
+      </NavLink>
     </Box>
   )
 }
